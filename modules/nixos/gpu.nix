9528d7c6b014631c9ccfffa0e7491b52a8c307bf
Yutong Chen <chenyutong007@gmail.com>
2025-09-16 12:42:25 +0800

    nvidia

--- modules/nixos/gpu.nix
+++ modules/nixos/gpu.nix
@@ -6,33 +6,24 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
+    open = true;
     modesetting.enable = true;
 
     powerManagement.enable = true;
     powerManagement.finegrained = true;
-    powerManagement.persistenceMode = true;
 
     prime = {
-      sync.enable = true;
-
       offload = {
         enable = true;
-        enableOffloadCmd = true; # 允许使用 `prime-run` 命令
+        enableOffloadCmd = true;
       };
 
-      # 为 AMD 集成显卡和 NVIDIA 独立显卡设置正确的 PCI 总线 ID
-      amdgpuBusId = "06:00.0";
-      nvidiaBusId = "01:00.0";
+      amdgpuBusId = "PCI:6@0:0:0";
+      nvidiaBusId = "PCI:1@0:0:0";
     };
 
     # 安装驱动包
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
-  # 配置 OpenGL 以支持混合显卡
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
 }
