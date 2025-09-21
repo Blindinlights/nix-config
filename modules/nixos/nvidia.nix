72b7b8929a0e61ca5dc84dce3ea37cbd052457b4
Yutong Chen <chenyutong007@gmail.com>
2025-09-21 19:14:23 +0800

    nvidia

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -14,4 +14,16 @@
     nvidiaBusId = "PCI:1@0:0:0";
     amdgpuBusId = "PCI:4@0:0:0";
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      linuxPackages = prev.linuxPackages.extend (
+        final: prev: {
+          nvidia_x11_open = prev.nvidia_x11_open.overrideAttrs {
+            src = /tmp/NVIDIA-Linux-x86_64-580.82.09.run;
+          };
+        }
+      );
+    })
+  ];
 }
