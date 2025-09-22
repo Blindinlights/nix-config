a4cb0a6226c839b1b6582b9d97049ff7d74cd78d
Yutong Chen <chenyutong007@gmail.com>
2025-09-22 10:04:41 +0800

    config

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -17,7 +17,7 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    waylandFrontend = true;
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-chinese-addons
       fcitx5-configtool
