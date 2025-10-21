263c4f2137e1b3cdc7418801b27a12f57fee2014
Yutong Chen <chenyutong007@gmail.com>
2025-10-21 11:41:01 +0800

    quickshell

--- modules/home-manager/common/git.nix
+++ modules/home-manager/common/git.nix
@@ -3,25 +3,24 @@
 {
   programs.git = {
     enable = true;
-    userName = "blindinlights";
-    userEmail = "chenyutong007@gmail.com";
-
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      cm = "commit -m";
-      ca = "commit --amend";
-      last = "log -1 HEAD";
-      unstage = "reset HEAD --";
-    };
-
-    extraConfig = {
+    settings = {
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        cm = "commit -m";
+        ca = "commit --amend";
+        last = "log -1 HEAD";
+        unstage = "reset HEAD --";
+      };
+      user.name = "blindinlights";
+      user.email = "chenyutong007@gmail.com";
       init.defaultBranch = "main";
       pull.rebase = false;
       color.ui = true;
     };
+
 
   };
 }
--- modules/home-manager/niri/noctalia.nix
+++ modules/home-manager/niri/noctalia.nix
@@ -1,4 +1,8 @@
 { pkgs, inputs, ... }:
+let
+
+  configRoot = ../../..;
+in
 {
   imports = [
     inputs.noctalia.homeModules.default
@@ -7,14 +11,66 @@
   home.packages = with pkgs; [
     inputs.noctalia.packages.${system}.default
   ];
-  programs.noctalia-shell = {
-    enable = true;
+  programs.noctalia-shell =
+    let
+      default-wallpaper = "${configRoot}/images/wallpapers/winxp.jpeg";
+      avatar-image = "${configRoot}/images/avatar.jpg";
+      wallpaper-dir = "${configRoot}/images/wallpapers";
+    in
 
-    # settings={
-    #   hooks={
-    #     enable=true;
-    #     darkmodeChange="darkman toggle";
-    #   };
-    # };
-  };
+    {
+      enable = true;
+
+      settings = {
+        general = {
+          avatarImage = avatar-image;
+          language = "zh-CN";
+
+        };
+
+        "bar" = {
+          backgroundOpacity = 0;
+          density = "default";
+          floating = false;
+          marginHorizontal = 0.25;
+          marginVertical = 0.25;
+          monitors = [ ];
+          position = "top";
+          showCapsule = true;
+          widgets = {
+            center = [ { "id" = "Clock"; } ];
+            left = [
+              { id = "Workspace"; }
+              { id = "ActiveWindow"; }
+              { id = "MediaMini"; }
+            ];
+            right = [
+              { id = "Tray"; }
+              { id = "Bluetooth"; }
+              { id = "WiFi"; }
+              { id = "NotificationHistory"; }
+              { id = "Battery"; }
+              { id = "Volume"; }
+              { id = "Brightness"; }
+              { id = "ControlCenter"; }
+            ];
+          };
+        };
+        location = {
+          name = "Xi'an";
+          showWeekNumberInCalendar = true;
+          use12hourFormat = false;
+          useFahrenheit = false;
+          weatherEnabled = true;
+        };
+        wallpaper = {
+          defaultWallpaper = default-wallpaper;
+          directory = wallpaper-dir;
+        };
+        hooks = {
+          enable = true;
+          darkmodeChange = "darkman toggle";
+        };
+      };
+    };
 }
